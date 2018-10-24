e2c4b5aa51a3eb6936a7de9a7280b1619e13a1db
Kaepaken <rush.courtneym@gmail.com>
2018-10-23 23:46:41 -0400

    Initial Commit

---
+++ SSMSTest/SSMSTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSMSTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection("Server=(LocalDB)\\LocalDBDemo;" +
+                                       "Initial Catalog=StudentTracker;");
+                SqlCommand cmd = new SqlCommand("Select * from Students");
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                conn.Open();
+                Console.WriteLine("Connection Open");
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(reader.GetOrdinal("Name")));
+                }
+
+                Console.ReadLine();
+                conn.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+    }
+}
